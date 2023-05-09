a67ee7cf2aafddec7202bdd85288491096f557d5
livewireza <92034037+livewireza@users.noreply.github.com>
2023-05-09 12:33:23 +0200

    Create content-validation.php
    content validation in PHP

---
+++ content-validation.php
@@ -0,0 +1,17 @@
+$fileName = basename($_FILES["uploadFile"]["name"]);
+$contentType = $_FILES['uploadFile']['type'];
+$MIMEtype = mime_content_type($_FILES['uploadFile']['tmp_name']);
+
+// whitelist test
+if (!preg_match('/^.*\.png$/', $fileName)) {
+    echo "Only PNG images are allowed";
+    die();
+}
+
+// content test
+foreach (array($contentType, $MIMEtype) as $type) {
+    if (!in_array($type, array('image/png'))) {
+        echo "Only SVG images are allowed";
+        die();
+    }
+}
